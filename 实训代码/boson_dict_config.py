a7c579e73b7279ef67915232d239b4e978a53abe
newmistic <631234919@qq.com>
2026-04-02 00:46:59 +0800

    first commit

---
+++ 实训代码/boson_dict_config.py
@@ -0,0 +1,9 @@
+class bonson_config:
+    stop_word_dir = 'stop.txt'
+    sentiment_dir1 = '正面评价词语（中文）1.txt'
+    sentiment_dir2 = '正面情感词语（中文）1.txt'
+    sentiment_dir3 = '负面评价词语（中文）1.txt'
+    sentiment_dir4 = '负面情感词语（中文）1.txt'
+    data = 'data.xlsx'
+    boson_dict_dir = 'BosonNLP_sentiment_score.txt'
+    negitive_words_dir = '否定词.txt'
